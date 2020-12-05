510660944d6a292481126979a1bcefded95b0183
charitha <64512048+sodumsucharitha@users.noreply.github.com>
2020-12-05 16:22:24 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World-final commit done ");
+		get("/hello", (req, res) -> "Hello World-final commit done today DEC 05 -2020 ");
 	}
 }
